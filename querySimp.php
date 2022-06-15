dd55cd1af642fde6788434a7f310e7a81e95443e
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-06-14 22:19:05 -0300

    Add files via upload

---
+++ querySimp.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "chaquebom";
+$conn = mysqli_connect($host, $user, $pass, $db);
+$results = $conn->query("select nome from sintomas where nome like '%$_POST[palavra]%'");
+
+?>
+
+<?php while($data = $results->fetch_assoc()): ?>
+
+    <li class="sugest-name"><?php echo $data['nome'] ?></li>
+
+<?php endwhile; ?>
