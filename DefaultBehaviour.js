06610ba9c77523949b5e9e4a580a8ce7105aa54c
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-06-16 20:43:47 -0300

    Add files via upload

---
+++ DefaultBehaviour.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    //default behaviour when clicking in the logo
+    $('.logo').click(function() {
+        $('.logo').fadeOut(500);
+        window.location.href = 'index.html';
+    });
+
+
+
+
+});
