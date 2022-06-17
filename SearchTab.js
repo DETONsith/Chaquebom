61e24a2551cc5e20cb5cdf16e99286e6bfc3785c
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-06-17 19:33:38 -0300

    Update SearchTab.js

--- SearchTab.js
+++ SearchTab.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
     $('#txtBusca').keyup(function() {
-
+        console.log("chegou aqui");
         var texto = $('#txtBusca').val();
+        console.log($('#txtBusca').val());
         $.post("querySimp.php",{palavra:texto},function(data){
             $(".sugests").html(data);
         })
