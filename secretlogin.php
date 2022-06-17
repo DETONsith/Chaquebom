0e5c8e9a9577141ed7582e49041067f3285d9124
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-06-17 19:00:44 -0300

    Update secretlogin.php

--- secretlogin.php
+++ secretlogin.php
@@ -3,6 +3,7 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SecretLogin</title>
 </head>
