73f36cb35dbe2388819b6990f40ab94c20ab6962
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-06-17 21:48:36 -0300

    Update DefaultBehaviour.js

--- DefaultBehaviour.js
+++ DefaultBehaviour.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-
+    
     //default behaviour when clicking in the logo
     $('.logo').click(function() {
-        $('.logo').fadeOut(500);
         window.location.href = 'index.html';
     });
 
