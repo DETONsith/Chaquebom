a0ca86387e3b043772f5ae2264ee78f97852e840
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-06-20 21:50:40 -0300

    Add files via upload

--- resultados.php
+++ resultados.php
@@ -9,7 +9,7 @@ $recipelist = [];
 
 foreach($sintomas as $sintoma){
 
-$results = $conn->query("select idReceita from REL_SintomaReceita where Sintoma_idSintoma like '$sintoma'");
+$results = $conn->query("select idReceita from REL_SintomaReceita where idSintoma_1 like '$sintoma'");
 
 //if idReceita is not in the array, add it
 foreach($results as $result){
@@ -17,6 +17,10 @@ foreach($results as $result){
         array_push($recipelist, $result['idReceita']);
     }
 }
+}
+
+foreach($recipelist as $recipinho){
+    $results = $conn->query("select * from RECEITA where idReceita like '$recipinho'");
 }
 
 ?>
@@ -40,22 +44,46 @@ foreach($results as $result){
                 <div class="content">
                     <div class="searchresult">
 
-                    <div><img src=></div>
+                        <?php
+                        //MOSTRA AS RECEITAS
+                        foreach($results as $result){
+                            echo "<div class='recipe'>";
+                            //NOME DA RECEITA
+                            echo "<div class='titleRecipe'>
+                            <h2><span class='recipeId'>".$result['idReceita']."</span> -> ".$result['nome']."</h2>
+                            </div>";
+                            
+
+
+                            //IMAGEM DA RECEITA
+                            echo "<div class='imageRecipe'>";
+                            echo "<img src='".$result['imagem']."' alt='".$result['nome']."'>
+                            </div>";
+                            //INGREDIENTES DA RECEITA
+                            echo "<div class='ingredientes'>
+                            <p>".$result['ingrediente']."</p> 
+                            </div>";
+
+                            echo "</div>";
+                        }
+                        
+                        ?>
 
                     </div>
+                    <div class="searchresultMenu">
                         <p>
                             <img src="goback_ico.png" class="icoclass">
                             <div class="pagesCount"></div>
                             <img src="next_ico.png" class="icoclass">
                         </p>
                     </div>
-
+                    </div>
+                    <div class="footer">
+                    </div>
                 </div>
 
-                <div class="footer">
-
-                </div>
-
-            </div>
+                
+        
+        </div>
     </body>
 </html>
