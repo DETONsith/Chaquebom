50cef3e60017a1fb6429c43eff4d6c697a2e496f
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-06-22 20:58:32 -0300

    Login system UPDATE

--- validation.php
+++ validation.php
@@ -5,17 +5,18 @@ require_once 'Connection.php';
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-$result = $conn->query("select * from logins where login = $login and password = $password");
+$result = $conn->query("select * from logins where login = '$login' and password = '$password'");
 
 if($result->num_rows > 0){ 
     session_start();
-    $_SESSION['AdmOnline'] = true; }
-else{ 
-    echo "<p id=error>Falha ao fazer login, usuário ou senha incorretos!</p>"
-    ?><script>
-        $(document).ready(function(){
-
-$.post("secretlogin.php",{denied:true,error:$('#error').val()});
-});
-    </script>
+    $_SESSION['AdmOnline'] = true;
+    header('Location: crud.php');
+}
+else{
+    echo '<form id="myForm" action="secretlogin.php" method="post"><input type="hidden" name="error"'.'" value="'.'Falha ao fazer login, usuário ou senha incorretos!'.'"> </form>';
+    ?>
 <?php } ?>
+
+<script type="text/javascript">
+    document.getElementById('myForm').submit();
+</script>
