bec2bd5fbe511a58e8e404d6b81da0d7b60c80a7
JaizaP <104856955+JaizaP@users.noreply.github.com>
2022-06-26 18:56:34 -0300

    Update CrudRecipes.php

--- CrudRecipes.php
+++ CrudRecipes.php
@@ -6,6 +6,9 @@ if($_SESSION['AdmOnline'] == false){
 else{
     echo "<h1>Bem-vindo, Administrador!</h1>";
 }
-
-
+<html>
+<div class="footerB">
+                <a href="index.html"><button>Voltar</button></a>
+        </div>
+</html>
 ?>
