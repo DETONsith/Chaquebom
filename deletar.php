b62762dbba8682c213ef30c7c58047c326507f84
Giovani Leal Gonçalves <65798725+DETONsith@users.noreply.github.com>
2022-07-03 11:06:38 -0300

    Update deletar.php

--- deletar.php
+++ deletar.php
@@ -1,8 +1,12 @@
 <?php
 require_once('conexao.php');
+require_once('connectionchecker.php');
 if(isset($_GET['deletid'])){
 
     $id=$_GET['deletid'];
+    $sql=$pdo->prepare("DELETE FROM rel_sintomareceita where idReceita = $id");
+    $sql->execute();
+    $sql = "";
     $sql=$pdo->prepare("DELETE FROM Receita WHERE idReceita=$id");
     $sql->execute();
     header('location:crud1.php');
